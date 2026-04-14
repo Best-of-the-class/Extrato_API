577d4b7460dc1a57645494596b9e714f2f615934
Luna Leão <lunaleao.maria@gmail.com>
2026-04-13 23:37:59 -0300

    :sparkles: feat: CRUD de login e cadastro de usuário completos com deploy

--- Extrato_API/Models/Usuario.cs
+++ Extrato_API/Models/Usuario.cs
@@ -1,15 +1,30 @@
-﻿namespace Extrato_API.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Extrato_API.Models
 {
+    [Table("tb_usuario")]
     public class Usuario
     {
+        // Id único para cada usuário
+        [Key]
+        [Column("id_usuario")]
         public Guid Id { get; set; } = Guid.NewGuid();
 
+        [Column("nome")]
         public string NomeUsuario { get; set; } = string.Empty;
 
+        [Column("email")]
         public string Email { get; set; } = string.Empty;
 
+        [Column("senha")]
         public string SenhaHash { get; set; } = string.Empty;
 
+        [Column("tipo_usuario")]
+        public string TipoUsuario { get; set; } = string.Empty;
+
+        [Column("criado_em")]
         public DateTime DataCadastro { get; set; } = DateTime.UtcNow;
 
         //Recuperação de senha
