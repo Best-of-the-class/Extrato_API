2cf04d3199cd9fa31097c57dc7b96684ea3b0e12
Luna Leão <lunaleao.maria@gmail.com>
2026-05-20 22:23:26 -0300

    :sparkles: feat: CRUD do dicionário e conserto de bug de login

---
+++ Extrato_API/Models/Dicionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Extrato_API.Models
+{
+    [Table("tb_dicionario")]
+    public class Dicionario
+    {
+        [Key]
+        [Column("id_dicionario")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("termo")]
+        public string Termo { get; set; } = string.Empty;
+
+        [Required]
+        [Column("definicao")]
+        public string Definicao { get; set; } = string.Empty;
+
+        [Column("criado_em")]
+        public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
+    }
+}
